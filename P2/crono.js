1214ce2c7e439dae8dcbc2ae3c726050312f8566
nuuriamr <n.mordillo.2023@gmail.com>
2026-03-17 14:20:21 +0100

    p2

--- P2/crono.js
+++ P2/crono.js
@@ -14,7 +14,7 @@ var sonidoExplosion = new Audio('explosion.mp3');
 var sonidoVictoria = new Audio('victoria.mp3');
 
 // 1. AL CARGAR: Arrancamos el juego
-window.onload = function() {
+window.onload = function () {
     crearTeclado();
     resetJuego();
 };
@@ -22,8 +22,8 @@ window.onload = function() {
 // 2. GENERAR TECLADO (0-9)
 function crearTeclado() {
     const contenedor = document.getElementById('teclado');
-    const numeros = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0] ;
-    
+    const numeros = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+
     numeros.forEach(n => {
         let btn = document.createElement('button');
         btn.innerText = n;
@@ -36,7 +36,7 @@ function crearTeclado() {
 // 3. LOGICA AL PULSAR UN NÚMERO
 function pulsarNumero(num) {
     if (!partidaActiva) return;
-
+    intentos_consumidos++;
     // Si es el primer toque, arranca el crono
     if (segundos === 0) iniciarCrono();
 
@@ -51,6 +51,7 @@ function pulsarNumero(num) {
             hueco.innerText = num;
             hueco.classList.add("acierto");
             acierto = true;
+
         }
     }
 
@@ -60,7 +61,6 @@ function pulsarNumero(num) {
     }
     // Gastar intento
     intentos--;
-    intentos_consumidos ++;
     document.getElementById("intentos-txt").innerText = intentos;
 
     comprobarFinal();
@@ -77,14 +77,14 @@ function comprobarFinal() {
     if (descubiertos === 4) {
         pararCrono();
         partidaActiva = false;
-        document.getElementById("mensaje").innerText = "¡VICTORIA! Tiempo: " + document.getElementById("crono").innerText +". Intentos consumidos: " + intentos_consumidos  + ". Intentos restantes: " + intentos;
+        document.getElementById("mensaje").innerText = "¡VICTORIA! Tiempo: " + document.getElementById("crono").innerText + ". Intentos consumidos: " + intentos_consumidos + ". Intentos restantes: " + intentos;
         sonidoVictoria.play();
-    } 
+    }
     else if (intentos <= 0) {
         pararCrono();
         partidaActiva = false;
-        document.getElementById("mensaje").innerText = "Perdiste pulsa RESET para volver a jugar. La clave era "+ claveSecreta.join("") +" Tiempo consumidos: " + document.getElementById("crono").innerText +". Intentos consumidos: " + intentos_consumidos  + ". Intentos restantes: " + intentos ;
-    sonidoExplosion.play();
+        document.getElementById("mensaje").innerText = "Perdiste pulsa RESET para volver a jugar. La clave era " + claveSecreta.join("") + " Tiempo consumidos: " + document.getElementById("crono").innerText + ". Intentos consumidos: " + intentos_consumidos + ". Intentos restantes: " + intentos;
+        sonidoExplosion.play();
     }
 }
 
@@ -93,30 +93,31 @@ function resetJuego() {
     pararCrono();
     segundos = 0;
     intentos = 7;
+    intentos_consumidos=0;
     partidaActiva = true;
     document.getElementById("crono").innerText = "0:00:00";
     document.getElementById("intentos-txt").innerText = intentos;
     document.getElementById("mensaje").innerText = "Nueva partida lista.";
-    
+
     // Generar 4 números distintos
     claveSecreta = [];
-    while(claveSecreta.length < 4) {
+    while (claveSecreta.length < 4) {
         let n = Math.floor(Math.random() * 10);
-        if(!claveSecreta.includes(n)) claveSecreta.push(n);
+        if (!claveSecreta.includes(n)) claveSecreta.push(n);
     }
-    console.log('clave:',claveSecreta)
+    console.log('clave:', claveSecreta)
 
     // Resetear visualmente
-    for(let i=0; i<4; i++) {
-        let h = document.getElementById("pos"+i);
+    for (let i = 0; i < 4; i++) {
+        let h = document.getElementById("pos" + i);
         h.innerText = "*";
         h.classList.remove("acierto");
     }
-    
+
     // Habilitar botones de nuevo
-    for(let i=0; i<=9; i++) {
-        let b = document.getElementById("btn-"+i);
-        if(b) b.disabled = false;
+    for (let i = 0; i <= 9; i++) {
+        let b = document.getElementById("btn-" + i);
+        if (b) b.disabled = false;
     }
 }
 
@@ -126,7 +127,7 @@ function iniciarCrono() {
         segundos++;
         let m = Math.floor(segundos / 60);
         let s = segundos % 60;
-        document.getElementById("crono").innerText = `0:${m}:${s < 10 ? '0'+s : s}`;
+        document.getElementById("crono").innerText = `0:${m}:${s < 10 ? '0' + s : s}`;
     }, 1000);
 }
 
